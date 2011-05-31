70c9d762c0518d312c2c621c1ecc5635ae5a17f5
Greg Brockman <gdb@gregbrockman.com>
2011-05-31 12:17:42 -0700

    Tweak curl failure error messages

--- stripe/__init__.py
+++ stripe/__init__.py
@@ -264,9 +264,9 @@ class APIRequestor(object):
     if e[0] in [pycurl.E_COULDNT_CONNECT,
                 pycurl.E_COULDNT_RESOLVE_HOST,
                 pycurl.E_OPERATION_TIMEOUTED]:
-      msg = "Could not connect to Stripe (%s).  Please check your internet connection and try again.  If this problem persists, you should check Stripe's service status at https://twitter.com/stripe, or let us know at support@stripe.com." % api_base
+      msg = "Could not connect to Stripe (%s).  Please check your internet connection and try again.  If this problem persists, you should check Stripe's service status at https://twitter.com/stripe, or let us know at support@stripe.com." % (api_base, )
     elif e[0] == pycurl.E_SSL_CACERT:
-      msg = "Could not verify Stripe's SSL certificate.  Please make sure that your network is not intercepting certificates.  (Try going to https://api.stripe.com in your browser.)  If this problem persists, let us know at support@stripe.com."
+      msg = "Could not verify Stripe's SSL certificate.  Please make sure that your network is not intercepting certificates.  (Try going to %s in your browser.)  If this problem persists, let us know at support@stripe.com." % (api_base, )
     else:
       msg = "Unexpected error communicating with Stripe.  If this problem persists, let us know at support@stripe.com."
     msg = textwrap.fill(msg) + "\n\n(Network error: " + e[1] + ")"
