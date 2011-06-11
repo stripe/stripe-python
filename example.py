d773bc4bc61a4507bd64474fe704891bf7a8109d
Greg Brockman <gdb@gregbrockman.com>
2011-06-10 17:13:41 -0700

    Change mnemonic -> description

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 import stripe
 stripe.api_key = '26OjYWkUnwDegILl9ZNVbefRjRboRSio'
 print "Attempting charge..."
-resp = stripe.Charge.execute(amount=200, currency='usd', card={'number' : '4242424242424242', 'exp_month' : 10, 'exp_year' : 2014}, mnemonic='customer@gmail.com')
+resp = stripe.Charge.execute(amount=200, currency='usd', card={'number' : '4242424242424242', 'exp_month' : 10, 'exp_year' : 2014}, description='customer@gmail.com')
 print 'Success: %r' % (resp, )
