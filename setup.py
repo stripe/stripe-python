e6c946eff9152ed4c7bbba1ace154e04b31c8da2
Greg Brockman <gdb@gregbrockman.com>
2011-06-12 23:46:26 -0700

    Change requires to install_requires

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(name='stripe',
       author_email='support@stripe.com',
       url='https://stripe.com/',
       packages=['stripe'],
-      requires=['json', 'pycurl']
+      install_requires=['json', 'pycurl']
 )
