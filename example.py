a36f186b88a041b9cf4f4b7090045ef378e77ae7
Patrick Collison <patrick@collison.ie>
2011-08-29 12:36:13 -0700

    fix example

--- example.py
+++ example.py
@@ -1,5 +1,18 @@
 import stripe
+
 stripe.api_key = '26OjYWkUnwDegILl9ZNVbefRjRboRSio'
+
 print "Attempting charge..."
-resp = stripe.Charge.execute(amount=200, currency='usd', card={'number' : '4242424242424242', 'exp_month' : 10, 'exp_year' : 2014}, description='customer@gmail.com')
+
+resp = stripe.Charge.create(
+         amount=200,
+         currency='usd',
+         card={
+           'number': '4242424242424242',
+           'exp_month': 10,
+           'exp_year': 2014
+         },
+         description='customer@gmail.com'
+       )
+
 print 'Success: %r' % (resp, )
