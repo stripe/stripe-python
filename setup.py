e15cf381307d79d227f4f0fab94f731591f6f639
Greg Brockman <gdb@gregbrockman.com>
2012-01-05 17:07:32 -0800

    Make install_requires into an array

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ path, script = os.path.split(sys.argv[0])
 os.chdir(os.path.abspath(path))
 
 # Get simplejson if we don't already have json
-install_requires = 'requests'
+install_requires = ['requests']
 try:
   importer.import_json()
 except ImportError:
