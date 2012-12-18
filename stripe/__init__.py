47d5ac70765a9fb43472c0599082e80a9ae377e8
Evan Broder <evan@stripe.com>
2012-12-18 01:03:25 -0800

    Fix requests version number check to work with requests 1.x.x (fixes #24)

--- stripe/__init__.py
+++ stripe/__init__.py
@@ -46,7 +46,7 @@ if not _httplib:
     # Probably some new-fangled version, so it should support verify
     pass
   else:
-    if minor < 8 or (minor == 8 and patch < 8):
+    if major < 1 and (minor < 8 or (minor == 8 and patch < 8)):
       print >>sys.stderr, 'Warning: the Stripe library requires that your Python "requests" library has a version no older than 0.8.8, but your "requests" library has version %s. Stripe will fall back to an alternate HTTP library, so everything should work, though we recommend upgrading your "requests" library. If you have any questions, please contact support@stripe.com. (HINT: running "pip install -U requests" should upgrade your requests library to the latest version.)' % (version, )
       _httplib = None
 
