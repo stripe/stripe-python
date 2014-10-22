6aab796f832fcee7469a2b1da4d0c9fcfc94f222
Stavros Korokithakis <stavros@korokithakis.net>
2014-10-22 18:22:24 +0300

    Add Bitcoin support.

--- stripe/__init__.py
+++ stripe/__init__.py
@@ -15,7 +15,7 @@ verify_ssl_certs = True
 # Resource
 from stripe.resource import (  # noqa
     Account, Balance, BalanceTransaction, Card, Charge, Customer, Invoice,
-    InvoiceItem, Plan, Token, Coupon, Event, Transfer, Recipient,
+    InvoiceItem, Payment, Plan, Token, Coupon, Event, Transfer, Recipient,
     ApplicationFee, Subscription)
 
 # Error imports.  Note that we may want to move these out of the root
