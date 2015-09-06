262ef69eb2cf4796ccc55d16fc0a4f6ba992ab92
Kubilay Kocak <koobs@users.noreply.github.com>
2015-09-06 21:20:38 +1000

    Stop LICENSE being installed in site-packages root

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     author_email='support@stripe.com',
     url='https://github.com/stripe/stripe-python',
     packages=['stripe', 'stripe.test'],
-    package_data={'stripe': ['data/ca-certificates.crt', '../VERSION']},
+    package_data={'stripe': ['data/ca-certificates.crt']},
     install_requires=install_requires,
     test_suite='stripe.test.all',
     use_2to3=True,
