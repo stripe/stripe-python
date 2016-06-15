37df23046069e104d0e60280521f50055a58394b
Kyle Conroy <kjc@stripe.com>
2016-06-15 14:24:30 -0700

    Disable modify on certain resources

--- stripe/resource.py
+++ stripe/resource.py
@@ -561,6 +561,14 @@ class Card(UpdateableAPIResource, DeletableAPIResource):
         return "%s/%s/%s/%s" % (base, owner_extn, class_base, extn)
 
     @classmethod
+    def modify(cls, sid, **params):
+        raise NotImplementedError(
+            "Can't modify a card without a customer, recipient or account "
+            "ID. Call save on customer.sources.retrieve('card_id'), "
+            "recipient.cards.retrieve('card_id'), or "
+            "account.external_accounts.retrieve('card_id') instead.")
+
+    @classmethod
     def retrieve(cls, id, api_key=None, stripe_account=None, **params):
         raise NotImplementedError(
             "Can't retrieve a card without a customer, recipient or account "
@@ -603,6 +611,13 @@ class BankAccount(UpdateableAPIResource, DeletableAPIResource, VerifyMixin):
                 "attached to a customer or an account." % token, 'id')
 
         return "%s/%s/%s/%s" % (base, owner_extn, class_base, extn)
+
+    @classmethod
+    def modify(cls, sid, **params):
+        raise NotImplementedError(
+            "Can't modify a bank account without a customer or account ID. "
+            "Call save on customer.sources.retrieve('bank_account_id') or "
+            "account.external_accounts.retrieve('bank_account_id') instead.")
 
     @classmethod
     def retrieve(cls, id, api_key=None, stripe_account=None, **params):
@@ -806,6 +821,12 @@ class Reversal(UpdateableAPIResource):
         cust_extn = urllib.quote_plus(transfer)
         extn = urllib.quote_plus(token)
         return "%s/%s/reversals/%s" % (base, cust_extn, extn)
+
+    @classmethod
+    def modify(cls, sid, **params):
+        raise NotImplementedError(
+            "Can't modify a reversal without a transfer"
+            "ID. Call save on transfer.reversals.retrieve('reversal_id')")
 
     @classmethod
     def retrieve(cls, id, api_key=None, **params):
