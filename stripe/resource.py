d8e9220764b373f3f4902b1371500cc4c2609adb
Kyle Conroy <kjc@stripe.com>
2016-06-15 13:51:07 -0700

    Add modify method for application fee

--- stripe/resource.py
+++ stripe/resource.py
@@ -859,13 +859,22 @@ class ApplicationFee(ListableAPIResource):
 
 class ApplicationFeeRefund(UpdateableAPIResource):
 
-    def instance_url(self):
-        token = util.utf8(self.id)
-        fee = util.utf8(self.fee)
+    @classmethod
+    def _build_instance_url(cls, fee, sid):
+        fee = util.utf8(fee)
+        sid = util.utf8(sid)
         base = ApplicationFee.class_url()
         cust_extn = urllib.quote_plus(fee)
-        extn = urllib.quote_plus(token)
+        extn = urllib.quote_plus(sid)
         return "%s/%s/refunds/%s" % (base, cust_extn, extn)
+
+    @classmethod
+    def modify(cls, fee, sid, **params):
+        url = cls._build_instance_url(fee, sid)
+        return cls._modify(url, **params)
+
+    def instance_url(self):
+        return self._build_instance_url(self.fee, self.id)
 
     @classmethod
     def retrieve(cls, id, api_key=None, **params):
