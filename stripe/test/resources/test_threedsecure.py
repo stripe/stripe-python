5ce48ae0ebef67f90b361978077e7dcbb200701c
Olivier Bellone <ob@stripe.com>
2016-07-12 12:08:54 +0200

    Added support for 3D Secure

---
+++ stripe/test/resources/test_threedsecure.py
@@ -0,0 +1,25 @@
+import stripe
+from stripe.test.helper import StripeResourceTest
+
+
+class ThreeDSecureTest(StripeResourceTest):
+
+    def test_threedsecure_create(self):
+        stripe.ThreeDSecure.create(
+            card="tok_test",
+            amount=1500,
+            currency="usd",
+            return_url="https://example.org/3d-secure-result"
+        )
+
+        self.requestor_mock.request.assert_called_with(
+            'post',
+            '/v1/3d_secure',
+            {
+                'card': 'tok_test',
+                'amount': 1500,
+                'currency': 'usd',
+                'return_url': 'https://example.org/3d-secure-result'
+            },
+            None
+        )
