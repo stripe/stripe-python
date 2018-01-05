00fc99ae0788607e4843e39fccb311465b0a918b
Olivier Bellone <ob@stripe.com>
2018-01-05 11:22:59 +0100

    Drop support for Python 2.6 and 3.3

--- setup.py
+++ setup.py
@@ -13,32 +13,12 @@ os.chdir(os.path.abspath(path))
 
 install_requires = []
 
-if sys.version_info < (2, 6):
-    warnings.warn(
-        'Python 2.5 is no longer officially supported by Stripe. '
-        'If you have any questions, please file an issue on Github or '
-        'contact us at support@stripe.com.',
-        DeprecationWarning)
-    install_requires.append('requests >= 0.8.8, < 0.10.1')
-    install_requires.append('ssl')
-else:
-    install_requires.append('requests >= 0.8.8')
-
-
 with open('LONG_DESCRIPTION.rst') as f:
     long_description = f.read()
 
 version_contents = {}
 with open(os.path.join('stripe', 'version.py')) as f:
     exec(f.read(), version_contents)
-
-# Get simplejson if we don't already have json
-if sys.version_info < (3, 0):
-    try:
-        from util import json
-    except ImportError:
-        install_requires.append('simplejson')
-
 
 setup(
     name='stripe',
@@ -62,10 +42,8 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
