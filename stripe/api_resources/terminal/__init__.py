af9b597686b3f7a01e5fe5bef63d49b48f7d2858
David Zhu <daz@stripe.com>
2018-09-17 14:35:52 -0700

    Bindings for Terminal endpoints

---
+++ stripe/api_resources/terminal/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function
+
+# flake8: noqa
+
+from stripe.api_resources.terminal.connection_token import ConnectionToken
+from stripe.api_resources.terminal.locations import Location
+from stripe.api_resources.terminal.readers import Reader
