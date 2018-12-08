7b1bdb07fe90a8414625a6d3658a1df43c49791a
Remi Jannel <remi@stripe.com>
2018-12-07 16:46:45 -0800

    Add support for the Account Link resource

---
+++ tests/api_resources/test_account_link.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, division, print_function
+
+import stripe
+
+
+class TestAccountLink(object):
+    def test_is_creatable(self, request_mock):
+        resource = stripe.AccountLink.create(
+            account='acct_123',
+            failure_url='https://stripe.com/failure',
+            success_url='https://stripe.com/success',
+            type='custom_account_verification'
+        )
+        request_mock.assert_requested(
+            'post',
+            '/v1/account_links'
+        )
+        assert isinstance(resource, stripe.AccountLink)
