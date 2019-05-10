937ca71701ff4f4adf9ea65073c64e03151fdece
Dmitry Korolev <dmitrykorolev@stripe.com>
2019-05-10 14:43:53 -0700

    Sort parent classes in AlipayAccount

--- stripe/api_resources/alipay_account.py
+++ stripe/api_resources/alipay_account.py
@@ -8,7 +8,7 @@ from stripe.api_resources.abstract import UpdateableAPIResource
 from stripe.six.moves.urllib.parse import quote_plus
 
 
-class AlipayAccount(UpdateableAPIResource, DeletableAPIResource):
+class AlipayAccount(DeletableAPIResource, UpdateableAPIResource):
     OBJECT_NAME = "alipay_account"
 
     @classmethod
