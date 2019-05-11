69541b15eb838a47cb86bdc1c3c959b3925a195c
Dmitry Korolev <dmitrykorolev@stripe.com>
2019-05-10 17:09:01 -0700

    Sort imports in Account

--- stripe/api_resources/account.py
+++ stripe/api_resources/account.py
@@ -3,10 +3,10 @@ from __future__ import absolute_import, division, print_function
 import stripe
 from stripe import oauth, six, util
 from stripe.api_resources.abstract import CreateableAPIResource
+from stripe.api_resources.abstract import custom_method
 from stripe.api_resources.abstract import DeletableAPIResource
 from stripe.api_resources.abstract import ListableAPIResource
 from stripe.api_resources.abstract import UpdateableAPIResource
-from stripe.api_resources.abstract import custom_method
 from stripe.api_resources.abstract import nested_resource_class_methods
 from stripe.six.moves.urllib.parse import quote_plus
 
