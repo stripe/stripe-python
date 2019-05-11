f0afa336d5b1df4db24cd299fc46defa18558843
Dmitry Korolev <dmitrykorolev@stripe.com>
2019-05-10 17:58:25 -0700

    Sort imports

--- stripe/api_resources/invoice.py
+++ stripe/api_resources/invoice.py
@@ -2,10 +2,10 @@ from __future__ import absolute_import, division, print_function
 
 from stripe import api_requestor, util
 from stripe.api_resources.abstract import CreateableAPIResource
+from stripe.api_resources.abstract import custom_method
 from stripe.api_resources.abstract import DeletableAPIResource
 from stripe.api_resources.abstract import ListableAPIResource
 from stripe.api_resources.abstract import UpdateableAPIResource
-from stripe.api_resources.abstract import custom_method
 
 
 @custom_method("finalize_invoice", http_verb="post", http_path="finalize")
--- stripe/api_resources/usage_record.py
+++ stripe/api_resources/usage_record.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
-from stripe.api_resources.abstract.api_resource import APIResource
 from stripe import api_requestor, util
+from stripe.api_resources.abstract.api_resource import APIResource
 
 
 class UsageRecord(APIResource):
