3e13fcd1b86bf36ba7edcc6fd6604128e9536bc1
Alex Rattray <rattrayalex@stripe.com>
2019-06-14 12:55:51 -1000

    Codegen for openapi f7fc004

--- stripe/api_resources/webhook_endpoint.py
+++ stripe/api_resources/webhook_endpoint.py
@@ -1,15 +1,8 @@
 from __future__ import absolute_import, division, print_function
 
 from stripe.api_resources.abstract import CreateableAPIResource
-from stripe.api_resources.abstract import DeletableAPIResource
 from stripe.api_resources.abstract import ListableAPIResource
-from stripe.api_resources.abstract import UpdateableAPIResource
 
 
-class WebhookEndpoint(
-    CreateableAPIResource,
-    DeletableAPIResource,
-    ListableAPIResource,
-    UpdateableAPIResource,
-):
+class WebhookEndpoint(CreateableAPIResource, ListableAPIResource):
     OBJECT_NAME = "webhook_endpoint"
