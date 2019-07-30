059181e103f49fe21fe7df2accfc8955f2781423
Remi Jannel <remi@stripe.com>
2019-07-30 09:58:16 -0700

    Move Balance History to /v1/balance_transactions and add tests

--- stripe/api_resources/balance_transaction.py
+++ stripe/api_resources/balance_transaction.py
@@ -5,7 +5,3 @@ from stripe.api_resources.abstract import ListableAPIResource
 
 class BalanceTransaction(ListableAPIResource):
     OBJECT_NAME = "balance_transaction"
-
-    @classmethod
-    def class_url(cls):
-        return "/v1/balance/history"
--- tests/api_resources/test_balance_transaction.py
+++ tests/api_resources/test_balance_transaction.py
@@ -9,6 +9,13 @@ TEST_RESOURCE_ID = "txn_123"
 class TestBalanceTransaction(object):
     def test_is_listable(self, request_mock):
         resources = stripe.BalanceTransaction.list()
-        request_mock.assert_requested("get", "/v1/balance/history")
+        request_mock.assert_requested("get", "/v1/balance_transactions")
         assert isinstance(resources.data, list)
         assert isinstance(resources.data[0], stripe.BalanceTransaction)
+
+    def test_is_retrievable(self, request_mock):
+        resource = stripe.BalanceTransaction.retrieve(TEST_RESOURCE_ID)
+        request_mock.assert_requested(
+            "get", "/v1/balance_transactions/%s" % TEST_RESOURCE_ID
+        )
+        assert isinstance(resource, stripe.BalanceTransaction)
