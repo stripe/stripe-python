e5a11a907d8b66e49fa793d490f97f4e925522bc
Remi Jannel <remi@stripe.com>
2020-09-02 12:33:37 -0700

    Add and fix tests

--- tests/api_resources/issuing/test_dispute.py
+++ tests/api_resources/issuing/test_dispute.py
@@ -8,7 +8,7 @@ TEST_RESOURCE_ID = "idp_123"
 
 class TestDispute(object):
     def test_is_creatable(self, request_mock):
-        resource = stripe.issuing.Dispute.create()
+        resource = stripe.issuing.Dispute.create(transaction="ipi_123")
         request_mock.assert_requested("post", "/v1/issuing/disputes")
         assert isinstance(resource, stripe.issuing.Dispute)
 
@@ -29,5 +29,12 @@ class TestDispute(object):
         resource = stripe.issuing.Dispute.retrieve(TEST_RESOURCE_ID)
         request_mock.assert_requested(
             "get", "/v1/issuing/disputes/%s" % TEST_RESOURCE_ID
+        )
+        assert isinstance(resource, stripe.issuing.Dispute)
+
+    def test_is_submittable(self, request_mock):
+        resource = stripe.issuing.Dispute.submit(TEST_RESOURCE_ID)
+        request_mock.assert_requested(
+            "post", "/v1/issuing/disputes/%s/submit" % TEST_RESOURCE_ID
         )
         assert isinstance(resource, stripe.issuing.Dispute)
