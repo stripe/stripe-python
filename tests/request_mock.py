921063393d2d194c111dd4ed3664f5e152776d8c
Dominic Charley-Roy <dcr@stripe.com>
2021-06-18 12:09:38 -0400

    Update support for @custom_method decorator.

--- tests/request_mock.py
+++ tests/request_mock.py
@@ -3,8 +3,8 @@ from __future__ import absolute_import, division, print_function
 import json
 
 import stripe
-from stripe import six
-from stripe.stripe_response import StripeResponse
+from stripe import six, util
+from stripe.stripe_response import StripeResponse, StripeStreamResponse
 
 
 class RequestMock(object):
@@ -12,6 +12,9 @@ class RequestMock(object):
         self._mocker = mocker
 
         self._real_request = stripe.api_requestor.APIRequestor.request
+        self._real_request_stream = (
+            stripe.api_requestor.APIRequestor.request_stream
+        )
         self._stub_request_handler = StubRequestHandler()
 
         self.constructor_patcher = self._mocker.patch(
@@ -26,16 +29,42 @@ class RequestMock(object):
             autospec=True,
         )
 
+        self.request_stream_patcher = self._mocker.patch(
+            "stripe.api_requestor.APIRequestor.request_stream",
+            side_effect=self._patched_request_stream,
+            autospec=True,
+        )
+
     def _patched_request(self, requestor, method, url, *args, **kwargs):
-        response = self._stub_request_handler.get_response(method, url)
+        response = self._stub_request_handler.get_response(
+            method, url, expect_stream=False
+        )
         if response is not None:
             return response, stripe.api_key
 
         return self._real_request(requestor, method, url, *args, **kwargs)
 
+    def _patched_request_stream(self, requestor, method, url, *args, **kwargs):
+        response = self._stub_request_handler.get_response(
+            method, url, expect_stream=True
+        )
+        if response is not None:
+            return response, stripe.api_key
+
+        return self._real_request_stream(
+            requestor, method, url, *args, **kwargs
+        )
+
     def stub_request(self, method, url, rbody={}, rcode=200, rheaders={}):
         self._stub_request_handler.register(
-            method, url, rbody, rcode, rheaders
+            method, url, rbody, rcode, rheaders, is_streaming=False
+        )
+
+    def stub_request_stream(
+        self, method, url, rbody={}, rcode=200, rheaders={}
+    ):
+        self._stub_request_handler.register(
+            method, url, rbody, rcode, rheaders, is_streaming=True
         )
 
     def assert_api_base(self, expected_api_base):
@@ -84,6 +113,16 @@ class RequestMock(object):
             raise AssertionError(msg)
 
     def assert_requested(self, method, url, params=None, headers=None):
+        self.assert_requested_internal(
+            self.request_patcher, method, url, params, headers
+        )
+
+    def assert_requested_stream(self, method, url, params=None, headers=None):
+        self.assert_requested_internal(
+            self.request_stream_patcher, method, url, params, headers
+        )
+
+    def assert_requested_internal(self, patcher, method, url, params, headers):
         params = params or self._mocker.ANY
         headers = headers or self._mocker.ANY
         called = False
@@ -99,7 +138,7 @@ class RequestMock(object):
 
         for args in possible_called_args:
             try:
-                self.request_patcher.assert_called_with(*args)
+                patcher.assert_called_with(*args)
             except AssertionError as e:
                 exception = e
             else:
@@ -117,23 +156,45 @@ class RequestMock(object):
             )
             raise AssertionError(msg)
 
+    def assert_no_request_stream(self):
+        if self.request_stream_patcher.call_count != 0:
+            msg = (
+                "Expected 'request_stream' to not have been called. "
+                "Called %s times." % (self.request_stream_patcher.call_count)
+            )
+            raise AssertionError(msg)
+
     def reset_mock(self):
         self.request_patcher.reset_mock()
+        self.request_stream_patcher.reset_mock()
 
 
 class StubRequestHandler(object):
     def __init__(self):
         self._entries = {}
 
-    def register(self, method, url, rbody={}, rcode=200, rheaders={}):
-        self._entries[(method, url)] = (rbody, rcode, rheaders)
+    def register(
+        self, method, url, rbody={}, rcode=200, rheaders={}, is_streaming=False
+    ):
+        self._entries[(method, url)] = (rbody, rcode, rheaders, is_streaming)
 
-    def get_response(self, method, url):
+    def get_response(self, method, url, expect_stream=False):
         if (method, url) in self._entries:
-            rbody, rcode, rheaders = self._entries.pop((method, url))
+            rbody, rcode, rheaders, is_streaming = self._entries.pop(
+                (method, url)
+            )
+
+            if expect_stream != is_streaming:
+                return None
+
             if not isinstance(rbody, six.string_types):
                 rbody = json.dumps(rbody)
-            stripe_response = StripeResponse(rbody, rcode, rheaders)
+            if is_streaming:
+                stripe_response = StripeStreamResponse(
+                    util.io.BytesIO(str.encode(rbody)), rcode, rheaders
+                )
+            else:
+                stripe_response = StripeResponse(rbody, rcode, rheaders)
             return stripe_response
 
         return None
