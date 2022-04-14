df58ac7709e4fe42904273eb3ab6a94ba9a90f1b
Richard Marmorstein <richardm@stripe.com>
2022-04-14 16:04:28 -0700

    Override

--- stripe/api_resources/funding_instructions.py
+++ stripe/api_resources/funding_instructions.py
@@ -8,3 +8,15 @@ from stripe.api_resources.abstract import ListableAPIResource
 class FundingInstructions(CreateableAPIResource, ListableAPIResource):
     OBJECT_NAME = "funding_instructions"
 
+    @classmethod
+    def create(cls, id, api_key=None, **params):
+        raise NotImplementedError(
+            "Can't create a funding instruction without a customer ID. Use customer.create_funding_instruction(...)"
+        )
+
+    @classmethod
+    def list(cls, id, api_key=None, **params):
+        raise NotImplementedError(
+            "Can't list funding instructions without a customer ID. Use customer.create_funding_instruction(...)"
+        )
+
