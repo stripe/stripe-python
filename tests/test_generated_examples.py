f750307b10d913c017a3032067753f2a23b0f492
Richard Marmorstein <richardm@stripe.com>
2022-05-05 12:19:05 -0700

    Codegen for openapi 0f6948c

--- tests/test_generated_examples.py
+++ tests/test_generated_examples.py
@@ -1402,53 +1402,6 @@ class TestGeneratedExamples(object):
         stripe.terminal.Reader.list(limit=3)
         request_mock.assert_requested("get", "/v1/terminal/readers")
 
-    def test_order_create(self, request_mock):
-        stripe.Order.create(
-            currency="usd",
-            email="jenny.rosen@example.com",
-            items=[{"type": "sku", "parent": "sku_xxxxxxxxxxxxx"}],
-            shipping={
-                "name": "Jenny Rosen",
-                "address": {
-                    "line1": "1234 Main Street",
-                    "city": "San Francisco",
-                    "state": "CA",
-                    "country": "US",
-                    "postal_code": "94111",
-                },
-            },
-        )
-        request_mock.assert_requested("post", "/v1/orders")
-
-    def test_order_retrieve(self, request_mock):
-        stripe.Order.retrieve("or_xxxxxxxxxxxxx")
-        request_mock.assert_requested("get", "/v1/orders/or_xxxxxxxxxxxxx")
-
-    def test_order_update(self, request_mock):
-        stripe.Order.modify("or_xxxxxxxxxxxxx", metadata={"order_id": "6735"})
-        request_mock.assert_requested("post", "/v1/orders/or_xxxxxxxxxxxxx")
-
-    def test_order_pay(self, request_mock):
-        stripe.Order.pay("or_xxxxxxxxxxxxx", source="tok_xxxx")
-        request_mock.assert_requested(
-            "post", "/v1/orders/or_xxxxxxxxxxxxx/pay"
-        )
-
-    def test_order_list(self, request_mock):
-        stripe.Order.list(limit=3)
-        request_mock.assert_requested("get", "/v1/orders")
-
-    def test_orderreturn_retrieve(self, request_mock):
-        stripe.OrderReturn.retrieve("orret_xxxxxxxxxxxxx")
-        request_mock.assert_requested(
-            "get",
-            "/v1/order_returns/orret_xxxxxxxxxxxxx",
-        )
-
-    def test_orderreturn_list(self, request_mock):
-        stripe.OrderReturn.list(limit=3)
-        request_mock.assert_requested("get", "/v1/order_returns")
-
     def test_sku_create(self, request_mock):
         stripe.SKU.create(
             attributes={"size": "Medium", "gender": "Unisex"},
@@ -1717,3 +1670,35 @@ class TestGeneratedExamples(object):
             "post",
             "/v1/test_helpers/refunds/re_123/expire",
         )
+
+    def test_order_create(self, request_mock):
+        stripe.Order.create(
+            description="description",
+            currency="usd",
+            line_items=[{"description": "my line item"}],
+        )
+        request_mock.assert_requested("post", "/v1/orders")
+
+    def test_order_update(self, request_mock):
+        stripe.Order.modify("order_xyz")
+        request_mock.assert_requested("post", "/v1/orders/order_xyz")
+
+    def test_order_list_line_items(self, request_mock):
+        stripe.Order.list_line_items("order_xyz")
+        request_mock.assert_requested("get", "/v1/orders/order_xyz/line_items")
+
+    def test_order_cancel(self, request_mock):
+        stripe.Order.cancel("order_xyz")
+        request_mock.assert_requested("post", "/v1/orders/order_xyz/cancel")
+
+    def test_order_reopen(self, request_mock):
+        stripe.Order.reopen("order_xyz")
+        request_mock.assert_requested("post", "/v1/orders/order_xyz/reopen")
+
+    def test_order_submit(self, request_mock):
+        stripe.Order.submit("order_xyz", expected_total=100)
+        request_mock.assert_requested("post", "/v1/orders/order_xyz/submit")
+
+    def test_order_update2(self, request_mock):
+        stripe.Order.modify("order_xyz")
+        request_mock.assert_requested("post", "/v1/orders/order_xyz")
