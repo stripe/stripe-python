86eabbb6cc7404fe901093b8c6e3fba68cbed1e5
Pavel Krymets <pakrym@stripe.com>
2022-07-25 11:00:34 -0700

    remove outdated tests

--- tests/api_resources/test_usage_record_summary.py
+++ tests/api_resources/test_usage_record_summary.py
@@ -5,8 +5,7 @@ import stripe
 
 class TestUsageRecordSummary(object):
     def test_is_listable(self, request_mock):
-        resource = stripe.SubscriptionItem.retrieve("si_123")
-        usage_record_summaries = resource.usage_record_summaries()
+        usage_record_summaries = stripe.SubscriptionItem.list_usage_record_summaries("si_123")
         request_mock.assert_requested(
             "get", "/v1/subscription_items/si_123/usage_record_summaries"
         )
