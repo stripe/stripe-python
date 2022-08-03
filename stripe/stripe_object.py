7e18d97707c0972db56a34ee183aff5834a719ae
Kamil Pajdzik <kamil@stripe.com>
2022-08-02 17:05:39 -0700

    Hotfix incorrect handling of additional request parameters

--- stripe/stripe_object.py
+++ stripe/stripe_object.py
@@ -250,10 +250,27 @@ class StripeObject(dict):
         headers=None,
         params=None,
     ):
+
         stripe_account = stripe_account or self.stripe_account
         stripe_version = stripe_version or self.stripe_version
         api_key = api_key or self.api_key
         params = params or self._retrieve_params
+
+        if api_key is None and 'api_key' in params:
+            api_key = params['api_key'];
+            del params['api_key']
+
+        if idempotency_key is None and 'idempotency_key' in params:
+            idempotency_key = params['idempotency_key'];
+            del params['idempotency_key']
+
+        if stripe_version is None and 'stripe_version' in params:
+            stripe_version = params['stripe_version'];
+            del params['stripe_version']
+
+        if stripe_account is None and 'stripe_account' in params:
+            stripe_account = params['stripe_account'];
+            del params['stripe_account']
 
         requestor = api_requestor.APIRequestor(
             key=api_key,
