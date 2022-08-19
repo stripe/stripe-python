df50cd470d30a38c8590bb1318601f07abac966a
Kamil Pajdzik <kamil@stripe.com>
2022-08-18 21:46:45 -0700

    Fix ASCII character

--- stripe/api_resources/treasury/financial_account.py
+++ stripe/api_resources/treasury/financial_account.py
@@ -14,7 +14,7 @@ class FinancialAccount(
 ):
     """
     Stripe Treasury provides users with a container for money called a FinancialAccount that is separate from their Payments balance.
-    FinancialAccounts serve as the source and destination of Treasury’s money movement APIs.
+    FinancialAccounts serve as the source and destination of Treasury's money movement APIs.
     """
 
     OBJECT_NAME = "treasury.financial_account"
