83f44c6888b78d3217c7f35adefabcce408bae8f
Pavel Krymets <pakrym@stripe.com>
2022-09-22 11:49:36 -0700

    Codegen for openapi v196

---
+++ stripe/api_resources/capital/financing_transaction.py
@@ -0,0 +1,12 @@
+# File generated from our OpenAPI spec
+from __future__ import absolute_import, division, print_function
+
+from stripe.api_resources.abstract import ListableAPIResource
+
+
+class FinancingTransaction(ListableAPIResource):
+    """
+    This is an object representing the details of a transaction on a Capital financing object.
+    """
+
+    OBJECT_NAME = "capital.financing_transaction"
