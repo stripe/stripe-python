d1cb7f6629e060fd2168ca4a00d099f3e501fac2
Pavel Krymets <pakrym@stripe.com>
2022-09-26 12:28:12 -0700

    Codegen for openapi v196

---
+++ stripe/api_resources/capital/__init__.py
@@ -0,0 +1,10 @@
+# File generated from our OpenAPI spec
+from __future__ import absolute_import, division, print_function
+
+# flake8: noqa
+
+from stripe.api_resources.capital.financing_offer import FinancingOffer
+from stripe.api_resources.capital.financing_summary import FinancingSummary
+from stripe.api_resources.capital.financing_transaction import (
+    FinancingTransaction,
+)
