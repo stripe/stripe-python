bdab77dcf008cde0eac060b5b28e903d3c62588c
Annie Li <anniel@stripe.com>
2022-10-24 16:09:20 -0700

    Revert back to docstring

--- stripe/api_resources/abstract/updateable_api_resource.py
+++ stripe/api_resources/abstract/updateable_api_resource.py
@@ -4,7 +4,6 @@ from stripe import util
 from stripe.api_resources.abstract.api_resource import APIResource
 from stripe.six.moves.urllib.parse import quote_plus
 
-import warnings
 
 class UpdateableAPIResource(APIResource):
     @classmethod
@@ -13,13 +12,11 @@ class UpdateableAPIResource(APIResource):
         return cls._static_request("post", url, params=params)
 
     def save(self, idempotency_key=None):
-        warnings.warn(
-            """
-            The `save` method is deprecated and will be removed in
-            a future major version of the library.
-            Use the class method `modify` on the resource instead.
-            """,
-            DeprecationWarning)
+        """
+        The `save` method is deprecated and will be removed in a future major version of the library.
+
+        Use the class method `modify` on the resource instead.
+        """
         updated_params = self.serialize(None)
         if updated_params:
             self._request_and_refresh(
