ebf838abc8bb5d9f611128bdb1cb28d50d5c870f
Annie Li <anniel@stripe.com>
2022-12-15 15:35:10 -0800

    Regenerate

---
+++ stripe/api_resources/tax_transaction.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# File generated from our OpenAPI spec
+from __future__ import absolute_import, division, print_function
+
+from stripe.api_resources.abstract import CreateableAPIResource
+
+
+class TaxTransaction(CreateableAPIResource):
+    """
+    A Tax `Transaction` records the tax collected from or refunded to your customer.
+    """
+
+    OBJECT_NAME = "tax.transaction"
+
+    @classmethod
+    def create_reversal(
+        cls, api_key=None, stripe_version=None, stripe_account=None, **params
+    ):
+        return cls._static_request(
+            "post",
+            "/v1/tax/transactions/create_reversal",
+            api_key=api_key,
+            stripe_version=stripe_version,
+            stripe_account=stripe_account,
+            params=params,
+        )
