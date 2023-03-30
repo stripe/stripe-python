da0fa44353e3d7293fbc96338a46a196a5c0f67a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-03-30 06:58:55 +0000

    Update generated code for v285

--- stripe/api_resources/tax/transaction.py
+++ stripe/api_resources/tax/transaction.py
@@ -3,10 +3,10 @@
 from __future__ import absolute_import, division, print_function
 
 from stripe import util
-from stripe.api_resources.abstract import CreateableAPIResource
+from stripe.api_resources.abstract import APIResource
 
 
-class Transaction(CreateableAPIResource):
+class Transaction(APIResource):
     """
     A Tax transaction records the tax collected from or refunded to your customer.
     """
