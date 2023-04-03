b5e3b8e4af056542a1d106b47f9ecf02888e6094
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-04-03 16:58:12 +0000

    Update generated code for v292

--- stripe/api_resources/account_session.py
+++ stripe/api_resources/account_session.py
@@ -7,13 +7,13 @@ from stripe.api_resources.abstract import CreateableAPIResource
 
 class AccountSession(CreateableAPIResource):
     """
-    An AccountSession allows a Connect platform to grant access to a connected account in Connect embedded UIs.
+    An AccountSession allows a Connect platform to grant access to a connected account in Connect embedded components.
 
-    We recommend that you create an AccountSession each time you need to display an embedded UI
+    We recommend that you create an AccountSession each time you need to display an embedded component
     to your user. Do not save AccountSessions to your database as they expire relatively
     quickly, and cannot be used more than once.
 
-    Related guide: [Connect embedded UIs](https://stripe.com/docs/connect/get-started-connect-embedded-uis).
+    Related guide: [Connect embedded components](https://stripe.com/docs/connect/get-started-connect-embedded-components).
     """
 
     OBJECT_NAME = "account_session"
