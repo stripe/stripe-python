95187c57be37f8b5a67887298d7986a24ba5081c
Richard Marmorstein <richardm@stripe.com>
2023-04-12 11:08:58 -0700

    Inline StripeObject._request
    Inlined StripeObject._request
    
    got rid of e.g. "api_version = api_version or self.api_version"
    
    No need for self.api_version as that is used only for "forwarding" the
    api version when you call a method from an response object. With
    services we won't do that (except for auto-pagination, which should be
    handled as a special case)

--- stripe/stripe_client.py
+++ stripe/stripe_client.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import, division, print_function
 
-from stripe import util
-from stripe.stripe_object import StripeObject
+from stripe import api_requestor, util
+
+# from stripe.stripe_object import StripeObject
 
 
 class StripeClient(object):
@@ -24,14 +25,36 @@ class StripeClient(object):
         headers=None,
         params=None,
     ):
-        obj = StripeObject()
-        return obj._request(
-            method_,
-            url_,
-            api_key,
-            idempotency_key,
-            stripe_version,
-            stripe_account,
-            headers,
-            params,
+        params = None if params is None else params.copy()
+        api_key = util.read_special_variable(params, "api_key", api_key)
+        idempotency_key = util.read_special_variable(
+            params, "idempotency_key", idempotency_key
+        )
+        stripe_version = util.read_special_variable(
+            params, "stripe_version", stripe_version
+        )
+        stripe_account = util.read_special_variable(
+            params, "stripe_account", stripe_account
+        )
+        headers = util.read_special_variable(params, "headers", headers)
+
+        stripe_account = stripe_account
+        stripe_version = stripe_version
+        api_key = api_key
+        params = params
+
+        requestor = api_requestor.APIRequestor(
+            key=api_key,
+            api_version=stripe_version,
+            account=stripe_account,
+        )
+
+        if idempotency_key is not None:
+            headers = {} if headers is None else headers.copy()
+            headers.update(util.populate_headers(idempotency_key))
+
+        response, api_key = requestor.request(method_, url_, params, headers)
+
+        return util.convert_to_stripe_object(
+            response, api_key, stripe_version, stripe_account, params
         )
