b165b18414e0a40a65b232c0e4699bd8ad5309c8
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-09-19 23:52:18 +0000

    Update generated code for v536

--- stripe/api_resources/file_link.py
+++ stripe/api_resources/file_link.py
@@ -26,7 +26,7 @@ class FileLink(
 ):
     """
     To share the contents of a `File` object with non-Stripe users, you can
-    create a `FileLink`. `FileLink`s contain a URL that can be used to
+    create a `FileLink`. `FileLink`s contain a URL that you can use to
     retrieve the contents of the file without authentication.
     """
 
