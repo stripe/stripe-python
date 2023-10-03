16d4c12e452c263e4a1aa3e46208e841ac048252
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-10-03 20:41:48 +0000

    Update generated code for v576

--- stripe/api_resources/issuing/__init__.py
+++ stripe/api_resources/issuing/__init__.py
@@ -8,4 +8,5 @@ from stripe.api_resources.issuing.authorization import Authorization
 from stripe.api_resources.issuing.card import Card
 from stripe.api_resources.issuing.cardholder import Cardholder
 from stripe.api_resources.issuing.dispute import Dispute
+from stripe.api_resources.issuing.token import Token
 from stripe.api_resources.issuing.transaction import Transaction
