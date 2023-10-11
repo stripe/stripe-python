e6c1fa839933369270a022a6d39ca3334cf24278
Annie Li <anniel@stripe.com>
2023-10-10 20:41:11 -0400

    Remove manual __future__ imports

--- stripe/__init__.py
+++ stripe/__init__.py
@@ -1,4 +1,3 @@
-from __future__ import absolute_import, division, print_function
 from typing_extensions import Literal
 from typing import Union, Optional
 
--- stripe/api_resources/abstract/__init__.py
+++ stripe/api_resources/abstract/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import, division, print_function
-
 # flake8: noqa
 
 from stripe.api_resources.abstract.api_resource import APIResource
