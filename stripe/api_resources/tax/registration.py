7debc723eab5c82be3e30ad538009931dab32c4e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-10-30 09:28:10 +0000

    Update generated code for v629

--- stripe/api_resources/tax/registration.py
+++ stripe/api_resources/tax/registration.py
@@ -1564,9 +1564,21 @@ class Registration(
             """
 
         class ListParams(RequestOptions):
+            ending_before: NotRequired["str|None"]
+            """
+            A cursor for use in pagination. `ending_before` is an object ID that defines your place in the list. For instance, if you make a list request and receive 100 objects, starting with `obj_bar`, your subsequent call can include `ending_before=obj_bar` in order to fetch the previous page of the list.
+            """
             expand: NotRequired["List[str]|None"]
             """
             Specifies which fields in the response should be expanded.
+            """
+            limit: NotRequired["int|None"]
+            """
+            A limit on the number of objects to be returned. Limit can range between 1 and 100, and the default is 10.
+            """
+            starting_after: NotRequired["str|None"]
+            """
+            A cursor for use in pagination. `starting_after` is an object ID that defines your place in the list. For instance, if you make a list request and receive 100 objects, ending with `obj_foo`, your subsequent call can include `starting_after=obj_foo` in order to fetch the next page of the list.
             """
             status: NotRequired[
                 "Literal['active', 'all', 'expired', 'scheduled']|None"
