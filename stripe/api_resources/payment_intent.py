0ebb6dd66de4c5d568d68cbf7678e592eb918b25
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-11-02 01:21:53 +0000

    Update generated code for v638

--- stripe/api_resources/payment_intent.py
+++ stripe/api_resources/payment_intent.py
@@ -256,6 +256,7 @@ class PaymentIntent(
                 "testmode_charges_only",
                 "tls_version_unsupported",
                 "token_already_used",
+                "token_card_network_invalid",
                 "token_in_use",
                 "transfer_source_balance_parameters_mismatch",
                 "transfers_not_allowed",
--- stripe/api_resources/quote_preview_invoice.py
+++ stripe/api_resources/quote_preview_invoice.py
@@ -455,6 +455,7 @@ class QuotePreviewInvoice(ListableAPIResource["QuotePreviewInvoice"]):
                 "testmode_charges_only",
                 "tls_version_unsupported",
                 "token_already_used",
+                "token_card_network_invalid",
                 "token_in_use",
                 "transfer_source_balance_parameters_mismatch",
                 "transfers_not_allowed",
