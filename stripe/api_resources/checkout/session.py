1c59c5cdc9ac27bf83f12b42c9b7047f051323a3
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-11-06 23:45:55 +0000

    Update generated code for v649

--- stripe/api_resources/checkout/session.py
+++ stripe/api_resources/checkout/session.py
@@ -1607,8 +1607,9 @@ class Session(
             """
             The URL to which Stripe should send customers when payment or setup
             is complete.
-            If you'd like to use information from the successful Checkout Session on your page,
-            read the guide on [customizing your success page](https://stripe.com/docs/payments/checkout/custom-success-page).
+            This parameter is not allowed if ui_mode is `embedded`. If you'd like to use
+            information from the successful Checkout Session on your page, read the
+            guide on [customizing your success page](https://stripe.com/docs/payments/checkout/custom-success-page).
             """
             tax_id_collection: NotRequired[
                 "Session.CreateParamsTaxIdCollection"
