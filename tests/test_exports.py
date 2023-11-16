b9c3a52a0980a299377f0858c77c170de89c0b61
Pavel Krymets <pakrym@stripe.com>
2023-11-16 10:10:42 -0800

    more tests

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -1,7 +1,6 @@
 # pyright: strict
 import stripe
 
-
 def test_can_import_stripe_object() -> None:
     from stripe.stripe_object import (
         StripeObject as StripeObjectFromStripeStripeObject,
@@ -10,6 +9,52 @@ def test_can_import_stripe_object() -> None:
     assert (
         stripe.stripe_object.StripeObject is StripeObjectFromStripeStripeObject
     )
+
+
+def test_can_import_webhook_members() -> None:
+    from stripe import (
+        Webhook,
+        WebhookSignature,
+    )
+
+    assert Webhook is not None
+    assert WebhookSignature is not None
+
+
+def test_can_import_abstract() -> None:
+    from stripe.api_resources.abstract import (APIResource as APIResourceFromApiResourcesAbstract)
+    from stripe.stripe_object import (
+        StripeObject,
+    )
+    assert APIResourceFromApiResourcesAbstract is stripe.abstract.APIResource[StripeObject]
+
+
+def test_can_import_app_info() -> None:
+    from stripe.app_info import (AppInfo as AppInfoFromStripeAppInfo)
+    from stripe import (AppInfo as AppInfoFromStripe)
+
+    assert AppInfoFromStripeAppInfo is AppInfoFromStripe
+    assert AppInfoFromStripeAppInfo is stripe.AppInfo
+
+
+def test_can_import_stripe_response() -> None:
+    from stripe.stripe_response import (StripeResponse as StripeResponseFromStripeResponse)
+
+    assert StripeResponseFromStripeResponse is stripe.stripe_response.StripeResponse
+
+def test_can_import_oauth_members() -> None:
+    from stripe import (
+        OAuth,
+    )
+
+    assert OAuth is not None
+
+def test_can_import_errors() -> None:
+    from stripe.error import (
+        StripeError as StripeErrorFromStripeError,
+    )
+
+    assert StripeErrorFromStripeError is not None
 
 
 def test_can_import_top_level_resource() -> None:
