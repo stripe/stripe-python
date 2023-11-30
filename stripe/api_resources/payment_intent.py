1006336362bd41d093d62ff8cdbd1c5f2a8dafc5
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-11-30 16:58:31 +0000

    Update generated code for v691

--- stripe/api_resources/payment_intent.py
+++ stripe/api_resources/payment_intent.py
@@ -1535,7 +1535,7 @@ class PaymentIntent(
             Request ability to [overcapture](https://stripe.com/docs/payments/overcapture) for this PaymentIntent.
             """
             request_three_d_secure: Optional[
-                Literal["any", "automatic", "challenge_only"]
+                Literal["any", "automatic", "challenge", "challenge_only"]
             ]
             """
             We strongly recommend that you rely on our SCA Engine to automatically prompt your customers for authentication based on risk level and [other requirements](https://stripe.com/docs/strong-customer-authentication). However, if you wish to request 3D Secure based on logic from your own fraud engine, provide this option. Permitted values include: `automatic` or `any`. If not provided, defaults to `automatic`. Read our guide on [manually requesting 3D Secure](https://stripe.com/docs/payments/3d-secure#manual-three-ds) for more information on how this configuration interacts with Radar and our SCA Engine.
