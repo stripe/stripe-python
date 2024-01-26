064640337213349b263b8d9f4f805415783a945d
Richard Marmorstein <richardm@stripe.com>
2024-01-25 16:46:31 -0800

    Fix

--- stripe/_api_requestor.py
+++ stripe/_api_requestor.py
@@ -445,13 +445,6 @@ class _APIRequestor(object):
             for key, value in supplied_headers.items():
                 headers[key] = value
 
-        log_info("Request to Stripe api", method=method, url=abs_url)
-        log_debug(
-            "Post details",
-            post_data=encoded_params,
-            api_version=request_options.get("stripe_version"),
-        )
-
         max_network_retries = request_options.get("max_network_retries")
 
         return (
@@ -510,9 +503,9 @@ class _APIRequestor(object):
                 rheaders,
             ) = self._get_http_client().request_stream_with_retries(
                 method,
-                url,
+                abs_url,
                 headers,
-                post_data=post_data,
+                post_data,
                 max_network_retries=max_network_retries,
                 _usage=_usage,
             )
@@ -523,9 +516,9 @@ class _APIRequestor(object):
                 rheaders,
             ) = self._get_http_client().request_with_retries(
                 method,
-                url,
+                abs_url,
                 headers,
-                post_data=post_data,
+                post_data,
                 max_network_retries=max_network_retries,
                 _usage=_usage,
             )
