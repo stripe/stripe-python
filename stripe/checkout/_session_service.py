e265d12f597f84a5700fe150257f8c7d67753458
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-01-30 00:32:14 +0000

    Update generated code for v800

--- stripe/checkout/_session_service.py
+++ stripe/checkout/_session_service.py
@@ -172,7 +172,7 @@ class SessionService(StripeService):
         Payment-method-specific configuration.
         """
         payment_method_types: NotRequired[
-            "List[Literal['acss_debit', 'affirm', 'afterpay_clearpay', 'alipay', 'au_becs_debit', 'bacs_debit', 'bancontact', 'blik', 'boleto', 'card', 'cashapp', 'customer_balance', 'eps', 'fpx', 'giropay', 'grabpay', 'ideal', 'klarna', 'konbini', 'link', 'oxxo', 'p24', 'paynow', 'paypal', 'pix', 'promptpay', 'revolut_pay', 'sepa_debit', 'sofort', 'us_bank_account', 'wechat_pay', 'zip']]"
+            "List[Literal['acss_debit', 'affirm', 'afterpay_clearpay', 'alipay', 'au_becs_debit', 'bacs_debit', 'bancontact', 'blik', 'boleto', 'card', 'cashapp', 'customer_balance', 'eps', 'fpx', 'giropay', 'grabpay', 'ideal', 'klarna', 'konbini', 'link', 'oxxo', 'p24', 'paynow', 'paypal', 'pix', 'promptpay', 'revolut_pay', 'sepa_debit', 'sofort', 'swish', 'us_bank_account', 'wechat_pay', 'zip']]"
         ]
         """
         A list of the types of payment methods (e.g., `card`) this Checkout Session can accept.
@@ -961,6 +961,12 @@ class SessionService(StripeService):
         """
         contains details about the Sofort payment method options.
         """
+        swish: NotRequired[
+            "SessionService.CreateParamsPaymentMethodOptionsSwish"
+        ]
+        """
+        contains details about the Swish payment method options.
+        """
         us_bank_account: NotRequired[
             "SessionService.CreateParamsPaymentMethodOptionsUsBankAccount"
         ]
@@ -1403,6 +1409,12 @@ class SessionService(StripeService):
         Providing this parameter will [attach the payment method](https://stripe.com/docs/payments/save-during-payment) to the PaymentIntent's Customer, if present, after the PaymentIntent is confirmed and any required actions from the user are complete. If no Customer was provided, the payment method can still be [attached](https://stripe.com/docs/api/payment_methods/attach) to a Customer after the transaction completes.
 
         When processing card payments, Stripe also uses `setup_future_usage` to dynamically optimize your payment flow and comply with regional legislation and network rules, such as [SCA](https://stripe.com/docs/strong-customer-authentication).
+        """
+
+    class CreateParamsPaymentMethodOptionsSwish(TypedDict):
+        reference: NotRequired["Literal['']|str"]
+        """
+        The order reference that will be displayed to customers in the Swish application. Defaults to the `id` of the Payment Intent.
         """
 
     class CreateParamsPaymentMethodOptionsUsBankAccount(TypedDict):
