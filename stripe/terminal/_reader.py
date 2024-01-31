450a2d7d60cd3415aac96194290e086487e87b9b
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-01-31 06:39:06 +0000

    Update generated code for v803

--- stripe/terminal/_reader.py
+++ stripe/terminal/_reader.py
@@ -524,7 +524,7 @@ class Reader(
     """
     Serial number of the reader.
     """
-    status: Optional[str]
+    status: Optional[Literal["offline", "online"]]
     """
     The networking status of the reader.
     """
