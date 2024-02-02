41962b11488de087f2b9dbb1fa81c215a270351e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-02-02 15:40:56 +0000

    Update generated code for v809

--- stripe/_confirmation_token.py
+++ stripe/_confirmation_token.py
@@ -191,7 +191,7 @@ class ConfirmationToken(APIResource["ConfirmationToken"]):
                 """
                 preferred: Optional[str]
                 """
-                The preferred network for the card. Can be `cartes_bancaires`, `mastercard`, `visa` or `invalid_preference` if requested network is not valid for the card.
+                The preferred network for co-branded cards. Can be `cartes_bancaires`, `mastercard`, `visa` or `invalid_preference` if requested network is not valid for the card.
                 """
 
             class ThreeDSecureUsage(StripeObject):
