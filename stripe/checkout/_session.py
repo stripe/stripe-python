6fb0a9ebde591ec142871d71f7f4463da59afb65
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-04 10:11:56 +0000

    Update generated code for v860

--- stripe/checkout/_session.py
+++ stripe/checkout/_session.py
@@ -641,6 +641,10 @@ class Session(
                 """
 
             installments: Optional[Installments]
+            request_three_d_secure: Literal["any", "automatic", "challenge"]
+            """
+            We strongly recommend that you rely on our SCA Engine to automatically prompt your customers for authentication based on risk level and [other requirements](https://stripe.com/docs/strong-customer-authentication). However, if you wish to request 3D Secure based on logic from your own fraud engine, provide this option. If not provided, this value defaults to `automatic`. Read our guide on [manually requesting 3D Secure](https://stripe.com/docs/payments/3d-secure/authentication-flow#manual-three-ds) for more information on how this configuration interacts with Radar and our SCA Engine.
+            """
             setup_future_usage: Optional[
                 Literal["none", "off_session", "on_session"]
             ]
@@ -2512,6 +2516,12 @@ class Session(
         ]
         """
         Installment options for card payments
+        """
+        request_three_d_secure: NotRequired[
+            "Literal['any', 'automatic', 'challenge']"
+        ]
+        """
+        We strongly recommend that you rely on our SCA Engine to automatically prompt your customers for authentication based on risk level and [other requirements](https://stripe.com/docs/strong-customer-authentication). However, if you wish to request 3D Secure based on logic from your own fraud engine, provide this option. If not provided, this value defaults to `automatic`. Read our guide on [manually requesting 3D Secure](https://stripe.com/docs/payments/3d-secure/authentication-flow#manual-three-ds) for more information on how this configuration interacts with Radar and our SCA Engine.
         """
         setup_future_usage: NotRequired["Literal['off_session', 'on_session']"]
         """
