377ab04422cf4300e89a973329e1848569974baa
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-06 22:56:09 +0000

    Update generated code for v866

--- stripe/_quote_preview_invoice.py
+++ stripe/_quote_preview_invoice.py
@@ -931,7 +931,8 @@ class QuotePreviewInvoice(StripeObject):
 
         metadata: Optional[Dict[str, str]]
         """
-        Set of [key-value pairs](https://stripe.com/docs/api/metadata) that will reflect the metadata of the subscription at the time of invoice creation. *Note: This attribute is populated only for invoices created on or after June 29, 2023.*
+        Set of [key-value pairs](https://stripe.com/docs/api/metadata) defined as subscription metadata when an invoice is created. Becomes an immutable snapshot of the subscription metadata at the time of invoice finalization.
+         *Note: This attribute is populated only for invoices created on or after June 29, 2023.*
         """
         pause_collection: Optional[PauseCollection]
         """
