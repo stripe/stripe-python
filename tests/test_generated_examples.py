7f0ffdf545dee2c71dbf84437a1005b9f6a0ce88
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-11 18:57:52 +0000

    Update generated code for v871

--- tests/test_generated_examples.py
+++ tests/test_generated_examples.py
@@ -2873,6 +2873,43 @@ class TestGeneratedExamples(object):
             post_data="settings[reconciliation_mode]=manual",
         )
 
+    def test_customers_cash_balance_transactions_get(
+        self, http_client_mock: HTTPClientMock
+    ) -> None:
+        stripe.Customer.list_cash_balance_transactions(
+            "cus_123",
+            limit=3,
+        )
+        http_client_mock.assert_requested(
+            "get",
+            path="/v1/customers/cus_123/cash_balance_transactions",
+            query_string="limit=3",
+        )
+
+    def test_customers_cash_balance_transactions_get_service(
+        self, http_client_mock: HTTPClientMock
+    ) -> None:
+        http_client_mock.stub_request(
+            "get",
+            "/v1/customers/cus_123/cash_balance_transactions",
+            "limit=3",
+        )
+        client = StripeClient(
+            "sk_test_123",
+            http_client=http_client_mock.get_mock_http_client(),
+        )
+
+        client.customers.cash_balance_transactions.list(
+            "cus_123",
+            {"limit": 3},
+        )
+        http_client_mock.assert_requested(
+            "get",
+            path="/v1/customers/cus_123/cash_balance_transactions",
+            query_string="limit=3",
+            api_base="https://api.stripe.com",
+        )
+
     def test_customers_delete(self, http_client_mock: HTTPClientMock) -> None:
         stripe.Customer.delete("cus_xxxxxxxxxxxxx")
         http_client_mock.assert_requested(
