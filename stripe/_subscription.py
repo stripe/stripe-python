406503a78e5e95c22515721abfb4969b9605468a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-12 15:21:15 +0000

    Update generated code for v872

--- stripe/_subscription.py
+++ stripe/_subscription.py
@@ -483,7 +483,7 @@ class Subscription(
         """
         A list of prices and quantities that will generate invoice items appended to the next invoice for this subscription. You may pass up to 20 items.
         """
-        application_fee_percent: NotRequired["float"]
+        application_fee_percent: NotRequired["Literal['']|float"]
         """
         A non-negative decimal between 0 and 100, with at most two decimal places. This represents the percentage of the subscription invoice total that will be transferred to the application owner's Stripe account. The request must be made by a platform account on a connected account in order to set an application fee percentage. For more information, see the application fees [documentation](https://stripe.com/docs/connect/subscriptions#collecting-fees-on-subscriptions).
         """
@@ -1375,7 +1375,7 @@ class Subscription(
         """
         A list of prices and quantities that will generate invoice items appended to the next invoice for this subscription. You may pass up to 20 items.
         """
-        application_fee_percent: NotRequired["float"]
+        application_fee_percent: NotRequired["Literal['']|float"]
         """
         A non-negative decimal between 0 and 100, with at most two decimal places. This represents the percentage of the subscription invoice total that will be transferred to the application owner's Stripe account. The request must be made by a platform account on a connected account in order to set an application fee percentage. For more information, see the application fees [documentation](https://stripe.com/docs/connect/subscriptions#collecting-fees-on-subscriptions).
         """
