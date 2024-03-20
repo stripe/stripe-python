7a6db95e2394f1a6b6614a9101a2327dfffc5ab9
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-20 22:58:08 +0000

    Update generated code for v890

--- stripe/_charge.py
+++ stripe/_charge.py
@@ -1513,6 +1513,10 @@ class Charge(
             """
             Last four digits of the bank account number.
             """
+            payment_reference: Optional[str]
+            """
+            Reference number to locate ACH payments with customer's bank.
+            """
             routing_number: Optional[str]
             """
             Routing number of the bank account.
