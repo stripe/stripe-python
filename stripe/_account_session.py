2e9dccd78ed4b1b70c86eb67c9b39eb19eb992c0
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-11 22:43:44 +0000

    Update generated code for v950

--- stripe/_account_session.py
+++ stripe/_account_session.py
@@ -21,6 +21,20 @@ class AccountSession(CreateableAPIResource["AccountSession"]):
     OBJECT_NAME: ClassVar[Literal["account_session"]] = "account_session"
 
     class Components(StripeObject):
+        class AccountManagement(StripeObject):
+            class Features(StripeObject):
+                external_account_collection: bool
+                """
+                Whether to allow platforms to control bank account collection for their connected accounts. This feature can only be false for custom accounts (or accounts where the platform is compliance owner). Otherwise, bank account collection is determined by compliance requirements.
+                """
+
+            enabled: bool
+            """
+            Whether the embedded component is enabled.
+            """
+            features: Features
+            _inner_class_types = {"features": Features}
+
         class AccountOnboarding(StripeObject):
             class Features(StripeObject):
                 external_account_collection: bool
@@ -49,6 +63,20 @@ class AccountSession(CreateableAPIResource["AccountSession"]):
         class Documents(StripeObject):
             class Features(StripeObject):
                 pass
+
+            enabled: bool
+            """
+            Whether the embedded component is enabled.
+            """
+            features: Features
+            _inner_class_types = {"features": Features}
+
+        class NotificationBanner(StripeObject):
+            class Features(StripeObject):
+                external_account_collection: bool
+                """
+                Whether to allow platforms to control bank account collection for their connected accounts. This feature can only be false for custom accounts (or accounts where the platform is compliance owner). Otherwise, bank account collection is determined by compliance requirements.
+                """
 
             enabled: bool
             """
@@ -131,16 +159,20 @@ class AccountSession(CreateableAPIResource["AccountSession"]):
             features: Features
             _inner_class_types = {"features": Features}
 
+        account_management: AccountManagement
         account_onboarding: AccountOnboarding
         capital_financing_promotion: Optional[CapitalFinancingPromotion]
         documents: Documents
+        notification_banner: NotificationBanner
         payment_details: PaymentDetails
         payments: Payments
         payouts: Payouts
         _inner_class_types = {
+            "account_management": AccountManagement,
             "account_onboarding": AccountOnboarding,
             "capital_financing_promotion": CapitalFinancingPromotion,
             "documents": Documents,
+            "notification_banner": NotificationBanner,
             "payment_details": PaymentDetails,
             "payments": Payments,
             "payouts": Payouts,
@@ -161,6 +193,12 @@ class AccountSession(CreateableAPIResource["AccountSession"]):
         """
 
     class CreateParamsComponents(TypedDict):
+        account_management: NotRequired[
+            "AccountSession.CreateParamsComponentsAccountManagement"
+        ]
+        """
+        Configuration for the account management embedded component.
+        """
         account_onboarding: NotRequired[
             "AccountSession.CreateParamsComponentsAccountOnboarding"
         ]
@@ -200,6 +238,12 @@ class AccountSession(CreateableAPIResource["AccountSession"]):
         """
         Configuration for the issuing cards list component.
         """
+        notification_banner: NotRequired[
+            "AccountSession.CreateParamsComponentsNotificationBanner"
+        ]
+        """
+        Configuration for the notification banner embedded component.
+        """
         payment_details: NotRequired[
             "AccountSession.CreateParamsComponentsPaymentDetails"
         ]
@@ -213,6 +257,24 @@ class AccountSession(CreateableAPIResource["AccountSession"]):
         payouts: NotRequired["AccountSession.CreateParamsComponentsPayouts"]
         """
         Configuration for the payouts embedded component.
+        """
+
+    class CreateParamsComponentsAccountManagement(TypedDict):
+        enabled: bool
+        """
+        Whether the embedded component is enabled.
+        """
+        features: NotRequired[
+            "AccountSession.CreateParamsComponentsAccountManagementFeatures"
+        ]
+        """
+        The list of features enabled in the embedded component.
+        """
+
+    class CreateParamsComponentsAccountManagementFeatures(TypedDict):
+        external_account_collection: NotRequired[bool]
+        """
+        Whether to allow platforms to control bank account collection for their connected accounts. This feature can only be false for custom accounts (or accounts where the platform is compliance owner). Otherwise, bank account collection is determined by compliance requirements.
         """
 
     class CreateParamsComponentsAccountOnboarding(TypedDict):
@@ -330,6 +392,24 @@ class AccountSession(CreateableAPIResource["AccountSession"]):
         cardholder_management: NotRequired[bool]
         """
         Whether to allow cardholder management features.
+        """
+
+    class CreateParamsComponentsNotificationBanner(TypedDict):
+        enabled: bool
+        """
+        Whether the embedded component is enabled.
+        """
+        features: NotRequired[
+            "AccountSession.CreateParamsComponentsNotificationBannerFeatures"
+        ]
+        """
+        The list of features enabled in the embedded component.
+        """
+
+    class CreateParamsComponentsNotificationBannerFeatures(TypedDict):
+        external_account_collection: NotRequired[bool]
+        """
+        Whether to allow platforms to control bank account collection for their connected accounts. This feature can only be false for custom accounts (or accounts where the platform is compliance owner). Otherwise, bank account collection is determined by compliance requirements.
         """
 
     class CreateParamsComponentsPaymentDetails(TypedDict):
