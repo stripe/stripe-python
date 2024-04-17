706d94862680b52d45992178c7515f2d5f340fd2
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-17 18:02:34 +0000

    Update generated code for v964

--- stripe/billing/_meter_event.py
+++ stripe/billing/_meter_event.py
@@ -31,7 +31,7 @@ class MeterEvent(CreateableAPIResource["MeterEvent"]):
         """
         payload: Dict[str, str]
         """
-        The payload of the event. This must contain a field with the event's numerical value and a field to map the event to a customer.
+        The payload of the event. This must contain the fields corresponding to a meter's `customer_mapping.event_payload_key` (default is `stripe_customer_id`) and `value_settings.event_payload_key` (default is `value`). Read more about the [payload](https://docs.stripe.com/billing/subscriptions/usage-based/recording-usage#payload-key-overrides).
         """
         timestamp: NotRequired[int]
         """
@@ -60,7 +60,7 @@ class MeterEvent(CreateableAPIResource["MeterEvent"]):
     """
     payload: Dict[str, str]
     """
-    The payload of the event.
+    The payload of the event. This contains the fields corresponding to a meter's `customer_mapping.event_payload_key` (default is `stripe_customer_id`) and `value_settings.event_payload_key` (default is `value`). Read more about the [payload](https://stripe.com/docs/billing/subscriptions/usage-based/recording-usage#payload-key-overrides).
     """
     timestamp: int
     """
