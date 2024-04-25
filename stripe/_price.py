08a83419842c877729d8ed8d33cbe9a919cb4bd0
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-25 01:14:51 +0000

    Update generated code for v991

--- stripe/_price.py
+++ stripe/_price.py
@@ -262,7 +262,7 @@ class Price(
         """
         unit_amount: NotRequired[int]
         """
-        A positive integer in cents (or local equivalent) (or 0 for a free price) representing how much to charge. One of `unit_amount` or `custom_unit_amount` is required, unless `billing_scheme=tiered`.
+        A positive integer in cents (or local equivalent) (or 0 for a free price) representing how much to charge. One of `unit_amount`, `unit_amount_decimal`, or `custom_unit_amount` is required, unless `billing_scheme=tiered`.
         """
         unit_amount_decimal: NotRequired[str]
         """
