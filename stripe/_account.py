d9a61ff6f3bf74f17b0d7616c1b12ffc726a6c96
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-25 00:11:17 +0000

    Update generated code for v990

--- stripe/_account.py
+++ stripe/_account.py
@@ -603,7 +603,7 @@ class Account(
                 "application_unified_accounts_beta",
             ]
             """
-            A value indicating the responsible payer of a bundle of Stripe fees for pricing-control eligible products on this account.
+            A value indicating the responsible payer of a bundle of Stripe fees for pricing-control eligible products on this account. Learn more about [fee behavior on connected accounts](https://docs.stripe.com/connect/direct-charges-fee-payer-behavior).
             """
 
         class Losses(StripeObject):
@@ -2250,7 +2250,7 @@ class Account(
     class CreateParamsControllerFees(TypedDict):
         payer: NotRequired[Literal["account", "application"]]
         """
-        A value indicating the responsible payer of Stripe fees on this account. Defaults to `account`.
+        A value indicating the responsible payer of Stripe fees on this account. Defaults to `account`. Learn more about [fee behavior on connected accounts](https://docs.stripe.com/connect/direct-charges-fee-payer-behavior).
         """
 
     class CreateParamsControllerLosses(TypedDict):
