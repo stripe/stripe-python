5da307ab7dbaee35ba64ca09707f6bcc2dc5a8b3
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-05-10 21:29:38 +0000

    Update generated code for v1027

--- stripe/entitlements/_feature_service.py
+++ stripe/entitlements/_feature_service.py
@@ -29,6 +29,10 @@ class FeatureService(StripeService):
         """
 
     class ListParams(TypedDict):
+        archived: NotRequired[bool]
+        """
+        If set, filter results to only include features with the given archive status.
+        """
         ending_before: NotRequired[str]
         """
         A cursor for use in pagination. `ending_before` is an object ID that defines your place in the list. For instance, if you make a list request and receive 100 objects, starting with `obj_bar`, your subsequent call can include `ending_before=obj_bar` in order to fetch the previous page of the list.
@@ -40,6 +44,10 @@ class FeatureService(StripeService):
         limit: NotRequired[int]
         """
         A limit on the number of objects to be returned. Limit can range between 1 and 100, and the default is 10.
+        """
+        lookup_key: NotRequired[str]
+        """
+        If set, filter results to only include features with the given lookup_key.
         """
         starting_after: NotRequired[str]
         """
