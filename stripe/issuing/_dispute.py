c7591f9f46ad73b6c84344e698022041e92d3ba5
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-05-14 19:31:37 +0000

    Update generated code for v1034

--- stripe/issuing/_dispute.py
+++ stripe/issuing/_dispute.py
@@ -894,6 +894,33 @@ class Dispute(
     """
     Has the value `true` if the object exists in live mode or the value `false` if the object exists in test mode.
     """
+    loss_reason: Optional[
+        Literal[
+            "cardholder_authentication_issuer_liability",
+            "eci5_token_transaction_with_tavv",
+            "excess_disputes_in_timeframe",
+            "has_not_met_the_minimum_dispute_amount_requirements",
+            "invalid_duplicate_dispute",
+            "invalid_incorrect_amount_dispute",
+            "invalid_no_authorization",
+            "invalid_use_of_disputes",
+            "merchandise_delivered_or_shipped",
+            "merchandise_or_service_as_described",
+            "not_cancelled",
+            "other",
+            "refund_issued",
+            "submitted_beyond_allowable_time_limit",
+            "transaction_3ds_required",
+            "transaction_approved_after_prior_fraud_dispute",
+            "transaction_authorized",
+            "transaction_electronically_read",
+            "transaction_qualifies_for_visa_easy_payment_service",
+            "transaction_unattended",
+        ]
+    ]
+    """
+    The enum that describes the dispute loss outcome. If the dispute is not lost, this field will be absent. New enum values may be added in the future, so be sure to handle unknown values.
+    """
     metadata: Dict[str, str]
     """
     Set of [key-value pairs](https://stripe.com/docs/api/metadata) that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
