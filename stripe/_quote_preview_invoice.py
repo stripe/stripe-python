3db112d353c9d92956aaae0a5972f96dbbe6b91a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-06-06 17:29:00 +0000

    Update generated code for v1065

--- stripe/_quote_preview_invoice.py
+++ stripe/_quote_preview_invoice.py
@@ -783,6 +783,7 @@ class QuotePreviewInvoice(StripeObject):
                     "sepa_credit_transfer",
                     "sepa_debit",
                     "sofort",
+                    "swish",
                     "us_bank_account",
                     "wechat_pay",
                 ]
