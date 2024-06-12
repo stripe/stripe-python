71eabb0bd52e5fcc8b3afb79e3eef293d74af0ff
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-06-12 19:39:09 +0000

    Update generated code for v1077

--- stripe/_balance_transaction.py
+++ stripe/_balance_transaction.py
@@ -24,6 +24,7 @@ if TYPE_CHECKING:
     )
     from stripe._dispute import Dispute as DisputeResource
     from stripe._payout import Payout
+    from stripe._platform_tax_fee import PlatformTaxFee
     from stripe._refund import Refund
     from stripe._reserve_transaction import ReserveTransaction
     from stripe._reversal import Reversal
@@ -192,6 +193,7 @@ class BalanceTransaction(ListableAPIResource["BalanceTransaction"]):
                 "IssuingDisputeResource",
                 "Transaction",
                 "Payout",
+                "PlatformTaxFee",
                 "Refund",
                 "ReserveTransaction",
                 "TaxDeductedAtSource",
