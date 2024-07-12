2460cf3f4f1636ae3dd3e430079b4090fc84eab4
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-07-12 17:00:03 +0000

    Update generated code for v1127

--- stripe/_subscription.py
+++ stripe/_subscription.py
@@ -405,7 +405,7 @@ class Subscription(
             Literal["off", "on_subscription"]
         ]
         """
-        Either `off`, or `on_subscription`. With `on_subscription` Stripe updates `subscription.default_payment_method` when a subscription payment succeeds.
+        Configure whether Stripe updates `subscription.default_payment_method` when payment succeeds. Defaults to `off`.
         """
         _inner_class_types = {"payment_method_options": PaymentMethodOptions}
 
@@ -1076,7 +1076,7 @@ class Subscription(
             Literal["off", "on_subscription"]
         ]
         """
-        Either `off`, or `on_subscription`. With `on_subscription` Stripe updates `subscription.default_payment_method` when a subscription payment succeeds.
+        Configure whether Stripe updates `subscription.default_payment_method` when payment succeeds. Defaults to `off` if unspecified.
         """
 
     class CreateParamsPaymentSettingsPaymentMethodOptions(TypedDict):
@@ -2026,7 +2026,7 @@ class Subscription(
             Literal["off", "on_subscription"]
         ]
         """
-        Either `off`, or `on_subscription`. With `on_subscription` Stripe updates `subscription.default_payment_method` when a subscription payment succeeds.
+        Configure whether Stripe updates `subscription.default_payment_method` when payment succeeds. Defaults to `off` if unspecified.
         """
 
     class ModifyParamsPaymentSettingsPaymentMethodOptions(TypedDict):
