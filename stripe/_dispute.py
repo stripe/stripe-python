3a10e1c97d3e5ee8f93b658048adec8df8fc958f
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-07-16 16:46:31 +0000

    Update generated code for v1131

--- stripe/_dispute.py
+++ stripe/_dispute.py
@@ -212,6 +212,9 @@ class Dispute(
         Only return disputes associated to the charge specified by this charge ID.
         """
         created: NotRequired["Dispute.ListParamsCreated|int"]
+        """
+        Only return disputes that were created during the given date interval.
+        """
         ending_before: NotRequired[str]
         """
         A cursor for use in pagination. `ending_before` is an object ID that defines your place in the list. For instance, if you make a list request and receive 100 objects, starting with `obj_bar`, your subsequent call can include `ending_before=obj_bar` in order to fetch the previous page of the list.
