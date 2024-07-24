f69094943fade143dc0bfaf114895bbd31d1c177
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-07-24 17:26:25 +0000

    Update generated code for v1149

--- stripe/_payment_method.py
+++ stripe/_payment_method.py
@@ -229,6 +229,10 @@ class PaymentMethod(
                     """
                     Card brand. Can be `amex`, `diners`, `discover`, `eftpos_au`, `jcb`, `mastercard`, `unionpay`, `visa`, or `unknown`.
                     """
+                    brand_product: Optional[str]
+                    """
+                    The [product code](https://stripe.com/docs/card-product-codes) that identifies the specific program or product associated with a card.
+                    """
                     capture_before: Optional[int]
                     """
                     When using manual capture, a future timestamp after which the charge will be automatically refunded if uncaptured.
@@ -290,6 +294,13 @@ class PaymentMethod(
                     network: Optional[str]
                     """
                     Identifies which network this charge was processed on. Can be `amex`, `cartes_bancaires`, `diners`, `discover`, `eftpos_au`, `interac`, `jcb`, `mastercard`, `unionpay`, `visa`, or `unknown`.
+                    """
+                    network_transaction_id: Optional[str]
+                    """
+                    This is used by the financial networks to identify a transaction.
+                    Visa calls this the Transaction ID, Mastercard calls this the Trace ID, and American Express calls this the Acquirer Reference Data.
+                    The first three digits of the Trace ID is the Financial Network Code, the next 6 digits is the Banknet Reference Number, and the last 4 digits represent the date (MM/DD).
+                    This field will be available for successful Visa, Mastercard, or American Express transactions and always null for other card brands.
                     """
                     offline: Optional[Offline]
                     """
@@ -648,6 +659,10 @@ class PaymentMethod(
         brand: Optional[str]
         """
         Card brand. Can be `amex`, `diners`, `discover`, `eftpos_au`, `jcb`, `mastercard`, `unionpay`, `visa`, or `unknown`.
+        """
+        brand_product: Optional[str]
+        """
+        The [product code](https://stripe.com/docs/card-product-codes) that identifies the specific program or product associated with a card.
         """
         cardholder_name: Optional[str]
         """
