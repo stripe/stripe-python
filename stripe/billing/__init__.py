85018b3150afe2f8a6ec6fbcbfd50ad8ae3f3c59
Ramya Rao <ramya@stripe.com>
2024-09-25 00:19:05 -0700

    Update generated code for 2024-09-30 release

--- stripe/billing/__init__.py
+++ stripe/billing/__init__.py
@@ -3,6 +3,22 @@
 from stripe.billing._alert import Alert as Alert
 from stripe.billing._alert_service import AlertService as AlertService
 from stripe.billing._alert_triggered import AlertTriggered as AlertTriggered
+from stripe.billing._credit_balance_summary import (
+    CreditBalanceSummary as CreditBalanceSummary,
+)
+from stripe.billing._credit_balance_summary_service import (
+    CreditBalanceSummaryService as CreditBalanceSummaryService,
+)
+from stripe.billing._credit_balance_transaction import (
+    CreditBalanceTransaction as CreditBalanceTransaction,
+)
+from stripe.billing._credit_balance_transaction_service import (
+    CreditBalanceTransactionService as CreditBalanceTransactionService,
+)
+from stripe.billing._credit_grant import CreditGrant as CreditGrant
+from stripe.billing._credit_grant_service import (
+    CreditGrantService as CreditGrantService,
+)
 from stripe.billing._meter import Meter as Meter
 from stripe.billing._meter_event import MeterEvent as MeterEvent
 from stripe.billing._meter_event_adjustment import (
