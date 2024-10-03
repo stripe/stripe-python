51d6844546b4dfa292ef360364e28ec468cc224b
Helen Ye <helenye@stripe.com>
2024-10-03 16:45:02 -0400

    tests

--- tests/test_v2_event.py
+++ tests/test_v2_event.py
@@ -18,7 +18,7 @@ class TestV2Event(object):
                 "id": "evt_234",
                 "object": "v2.core.event",
                 "type": "financial_account.balance.opened",
-                "livemode": "false",
+                "livemode": True,
                 "created": "2022-02-15T00:27:45.330Z",
                 "related_object": {
                     "id": "fa_123",
@@ -40,7 +40,7 @@ class TestV2Event(object):
                 "id": "evt_234",
                 "object": "v2.core.event",
                 "type": "financial_account.balance.opened",
-                "livemode": "false",
+                "livemode": False,
                 "created": "2022-02-15T00:27:45.330Z",
                 "related_object": {
                     "id": "fa_123",
