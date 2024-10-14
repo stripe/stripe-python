a6001fae3eee47e5e10f730018e61f66bb847303
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-10-14 19:06:45 +0000

    Update generated code for v1293

--- stripe/issuing/_authorization_service.py
+++ stripe/issuing/_authorization_service.py
@@ -63,9 +63,7 @@ class AuthorizationService(StripeService):
         """
         A cursor for use in pagination. `starting_after` is an object ID that defines your place in the list. For instance, if you make a list request and receive 100 objects, ending with `obj_foo`, your subsequent call can include `starting_after=obj_foo` in order to fetch the next page of the list.
         """
-        status: NotRequired[
-            Literal["closed", "expired", "pending", "reversed"]
-        ]
+        status: NotRequired[Literal["closed", "pending", "reversed"]]
         """
         Only return authorizations with the given status. One of `pending`, `closed`, or `reversed`.
         """
