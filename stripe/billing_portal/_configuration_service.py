a02b80355821712706f705dd3175baf657189ccd
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-10-18 17:04:19 +0000

    Update generated code for v1314

--- stripe/billing_portal/_configuration_service.py
+++ stripe/billing_portal/_configuration_service.py
@@ -177,6 +177,12 @@ class ConfigurationService(StripeService):
         """
         Determines how to handle prorations resulting from subscription updates. Valid values are `none`, `create_prorations`, and `always_invoice`.
         """
+        schedule_at_period_end: NotRequired[
+            "ConfigurationService.CreateParamsFeaturesSubscriptionUpdateScheduleAtPeriodEnd"
+        ]
+        """
+        Setting to control when an update should be scheduled at the end of the period instead of applying immediately.
+        """
 
     class CreateParamsFeaturesSubscriptionUpdateProduct(TypedDict):
         prices: List[str]
@@ -186,6 +192,24 @@ class ConfigurationService(StripeService):
         product: str
         """
         The product id.
+        """
+
+    class CreateParamsFeaturesSubscriptionUpdateScheduleAtPeriodEnd(TypedDict):
+        conditions: NotRequired[
+            List[
+                "ConfigurationService.CreateParamsFeaturesSubscriptionUpdateScheduleAtPeriodEndCondition"
+            ]
+        ]
+        """
+        List of conditions. When any condition is true, the update will be scheduled at the end of the current period.
+        """
+
+    class CreateParamsFeaturesSubscriptionUpdateScheduleAtPeriodEndCondition(
+        TypedDict,
+    ):
+        type: Literal["decreasing_item_amount", "shortening_interval"]
+        """
+        The type of condition.
         """
 
     class CreateParamsLoginPage(TypedDict):
@@ -385,6 +409,12 @@ class ConfigurationService(StripeService):
         """
         Determines how to handle prorations resulting from subscription updates. Valid values are `none`, `create_prorations`, and `always_invoice`.
         """
+        schedule_at_period_end: NotRequired[
+            "ConfigurationService.UpdateParamsFeaturesSubscriptionUpdateScheduleAtPeriodEnd"
+        ]
+        """
+        Setting to control when an update should be scheduled at the end of the period instead of applying immediately.
+        """
 
     class UpdateParamsFeaturesSubscriptionUpdateProduct(TypedDict):
         prices: List[str]
@@ -394,6 +424,22 @@ class ConfigurationService(StripeService):
         product: str
         """
         The product id.
+        """
+
+    class UpdateParamsFeaturesSubscriptionUpdateScheduleAtPeriodEnd(TypedDict):
+        conditions: NotRequired[
+            "Literal['']|List[ConfigurationService.UpdateParamsFeaturesSubscriptionUpdateScheduleAtPeriodEndCondition]"
+        ]
+        """
+        List of conditions. When any condition is true, the update will be scheduled at the end of the current period.
+        """
+
+    class UpdateParamsFeaturesSubscriptionUpdateScheduleAtPeriodEndCondition(
+        TypedDict,
+    ):
+        type: Literal["decreasing_item_amount", "shortening_interval"]
+        """
+        The type of condition.
         """
 
     class UpdateParamsLoginPage(TypedDict):
