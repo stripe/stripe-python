659c33e47d7224cfa05472ee28bdda5fc82b9c9a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-11-01 09:27:16 +0000

    Update generated code for v1323

--- stripe/_payment_link.py
+++ stripe/_payment_link.py
@@ -1702,6 +1702,10 @@ class PaymentLink(
         """
         Configuration for collecting the customer's shipping address.
         """
+        submit_type: NotRequired[Literal["auto", "book", "donate", "pay"]]
+        """
+        Describes the type of transaction being performed in order to customize relevant text on the page, such as the submit button. Changing this value will also affect the hostname in the [url](https://stripe.com/docs/api/payment_links/payment_links/object#url) property (example: `donate.stripe.com`).
+        """
         subscription_data: NotRequired[
             "PaymentLink.ModifyParamsSubscriptionData"
         ]
--- stripe/_payment_link_service.py
+++ stripe/_payment_link_service.py
@@ -1045,6 +1045,10 @@ class PaymentLinkService(StripeService):
         """
         Configuration for collecting the customer's shipping address.
         """
+        submit_type: NotRequired[Literal["auto", "book", "donate", "pay"]]
+        """
+        Describes the type of transaction being performed in order to customize relevant text on the page, such as the submit button. Changing this value will also affect the hostname in the [url](https://stripe.com/docs/api/payment_links/payment_links/object#url) property (example: `donate.stripe.com`).
+        """
         subscription_data: NotRequired[
             "PaymentLinkService.UpdateParamsSubscriptionData"
         ]
