313de45d617beff935cd86b9b7aa3c61f387c11b
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-11-15 22:29:32 +0000

    Update generated code for v1359

--- stripe/terminal/_location.py
+++ stripe/terminal/_location.py
@@ -124,7 +124,7 @@ class Location(
     class ModifyParams(RequestOptions):
         address: NotRequired["Location.ModifyParamsAddress"]
         """
-        The full address of the location. If you're updating the `address` field, avoid changing the `country`. If you need to modify the `country` field, create a new `Location` object and re-register any existing readers to that location.
+        The full address of the location. You can't change the location's `country`. If you need to modify the `country` field, create a new `Location` object and re-register any existing readers to that location.
         """
         configuration_overrides: NotRequired["Literal['']|str"]
         """
