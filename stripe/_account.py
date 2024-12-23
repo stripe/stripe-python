bc15d350dafe276e15d7f2a8edf6af9f15d3f690
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-12-23 21:09:18 +0000

    Update generated code for v1423

--- stripe/_account.py
+++ stripe/_account.py
@@ -548,6 +548,20 @@ class Account(
             Town/cho-me.
             """
 
+        class DirectorshipDeclaration(StripeObject):
+            date: Optional[int]
+            """
+            The Unix timestamp marking when the directorship declaration attestation was made.
+            """
+            ip: Optional[str]
+            """
+            The IP address from which the directorship declaration attestation was made.
+            """
+            user_agent: Optional[str]
+            """
+            The user-agent string from the browser where the directorship declaration attestation was made.
+            """
+
         class OwnershipDeclaration(StripeObject):
             date: Optional[int]
             """
@@ -597,6 +611,7 @@ class Account(
         """
         Whether the company's directors have been provided. This Boolean will be `true` if you've manually indicated that all directors are provided via [the `directors_provided` parameter](https://stripe.com/docs/api/accounts/update#update_account-company-directors_provided).
         """
+        directorship_declaration: Optional[DirectorshipDeclaration]
         executives_provided: Optional[bool]
         """
         Whether the company's executives have been provided. This Boolean will be `true` if you've manually indicated that all executives are provided via [the `executives_provided` parameter](https://stripe.com/docs/api/accounts/update#update_account-company-executives_provided), or if Stripe determined that sufficient executives were provided.
@@ -683,6 +698,7 @@ class Account(
             "address": Address,
             "address_kana": AddressKana,
             "address_kanji": AddressKanji,
+            "directorship_declaration": DirectorshipDeclaration,
             "ownership_declaration": OwnershipDeclaration,
             "verification": Verification,
         }
