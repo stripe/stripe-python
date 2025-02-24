03cc3fde8f412976df3a88592ed38417a211163d
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-02-24 19:25:06 +0000

    Update generated code for v1505

--- stripe/v2/_event_destination.py
+++ stripe/v2/_event_destination.py
@@ -6,10 +6,6 @@ from typing_extensions import Literal
 
 
 class EventDestination(StripeObject):
-    """
-    Set up an event destination to receive events from Stripe across multiple destination types, including [webhook endpoints](https://docs.stripe.com/webhooks) and [Amazon EventBridge](https://docs.stripe.com/event-destinations/eventbridge). Event destinations support receiving [thin events](https://docs.stripe.com/api/v2/events) and [snapshot events](https://docs.stripe.com/api/events).
-    """
-
     OBJECT_NAME: ClassVar[Literal["v2.core.event_destination"]] = (
         "v2.core.event_destination"
     )
