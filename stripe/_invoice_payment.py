1a157af5febc4ace0fa52421f0d474587da04797
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-03-20 04:19:24 +0000

    Update generated code for v1590

--- stripe/_invoice_payment.py
+++ stripe/_invoice_payment.py
@@ -49,10 +49,6 @@ class InvoicePayment(StripeObject):
         The time that the payment succeeded.
         """
 
-    amount_overpaid: Optional[int]
-    """
-    Excess payment that was received for this invoice and credited to the customer's `invoice_credit_balance`. This field is null until the payment is `paid`. Overpayment can happen when you attach more than one PaymentIntent to the invoice, and each of them succeeds. To avoid overpayment, cancel any PaymentIntents that you do not need before attaching more.
-    """
     amount_paid: Optional[int]
     """
     Amount that was actually paid for this invoice, in cents (or local equivalent). This field is null until the payment is `paid`. This amount can be less than the `amount_requested` if the PaymentIntent's `amount_received` is not sufficient to pay all of the invoices that it is attached to.
