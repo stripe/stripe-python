318c637f411f57f4375bda762894f8f51cf89b02
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-04-11 14:27:05 +0000

    Update generated code for v1672

--- stripe/capital/_financing_summary.py
+++ stripe/capital/_financing_summary.py
@@ -26,16 +26,16 @@ class FinancingSummary(SingletonAPIResource["FinancingSummary"]):
             """
             paid_amount: Optional[int]
             """
-            The amount that has already been paid in the current repayment interval.
+            The amount that has already been paid in the current repayment interval, in minor units. For example, $100 USD will be represented as 10000.
             """
             remaining_amount: int
             """
-            The amount that is yet to be paid in the current repayment interval.
+            The amount that is yet to be paid in the current repayment interval, in minor units. For example, $100 USD will be represented as 10000.
             """
 
         advance_amount: int
         """
-        Amount of financing offered, in minor units.
+        Amount of financing offered, in minor units. For example, $1,000 USD will be represented as 100000.
         """
         advance_paid_out_at: Optional[float]
         """
@@ -51,15 +51,15 @@ class FinancingSummary(SingletonAPIResource["FinancingSummary"]):
         """
         fee_amount: int
         """
-        Fixed fee amount, in minor units.
+        Fixed fee amount, in minor units. For example, $100 USD will be represented as 10000.
         """
         paid_amount: int
         """
-        The amount the Connected account has paid toward the financing debt so far.
+        The amount the Connected account has paid toward the financing debt so far, in minor units. For example, $1,000 USD will be represented as 100000.
         """
         remaining_amount: int
         """
-        The balance remaining to be paid on the financing, in minor units.
+        The balance remaining to be paid on the financing, in minor units. For example, $1,000 USD will be represented as 100000.
         """
         repayments_begin_at: Optional[float]
         """
