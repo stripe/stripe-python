54b450bd858d6efd1f0cf71c58138402ea14868a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-04-22 21:50:00 +0000

    Update generated code for v1718

--- stripe/v2/billing/_meter_event_session.py
+++ stripe/v2/billing/_meter_event_session.py
@@ -26,11 +26,11 @@ class MeterEventSession(StripeObject):
     """
     The unique id of this auth session.
     """
-    livemode: bool
-    """
-    Has the value `true` if the object exists in live mode or the value `false` if the object exists in test mode.
-    """
     object: Literal["v2.billing.meter_event_session"]
     """
     String representing the object's type. Objects of the same type share the same value of the object field.
+    """
+    livemode: bool
+    """
+    Has the value `true` if the object exists in live mode or the value `false` if the object exists in test mode.
     """
