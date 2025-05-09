e7d6f101ec8f5efdc8766bd7e1ecfa9ad9d0519a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-05-09 18:46:42 +0000

    Update generated code for v1743

--- stripe/checkout/_session.py
+++ stripe/checkout/_session.py
@@ -4233,6 +4233,10 @@ class Session(
         """
         Uses the `allow_redisplay` value of each saved payment method to filter the set presented to a returning customer. By default, only saved payment methods with 'allow_redisplay: ‘always' are shown in Checkout.
         """
+        payment_method_remove: NotRequired[Literal["disabled", "enabled"]]
+        """
+        Enable customers to choose if they wish to remove their saved payment methods. Disabled by default.
+        """
         payment_method_save: NotRequired[Literal["disabled", "enabled"]]
         """
         Enable customers to choose if they wish to save their payment method for future use. Disabled by default.
