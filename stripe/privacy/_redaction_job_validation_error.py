438dc11e47940df5162142a4296adb929d5c7e69
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-05-16 20:49:50 +0000

    Update generated code for v1769

--- stripe/privacy/_redaction_job_validation_error.py
+++ stripe/privacy/_redaction_job_validation_error.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # File generated from our OpenAPI spec
 from stripe._stripe_object import StripeObject
-from typing import ClassVar, Dict, Optional
+from typing import ClassVar, Optional
 from typing_extensions import Literal
 
 
@@ -15,6 +15,17 @@ class RedactionJobValidationError(StripeObject):
     OBJECT_NAME: ClassVar[
         Literal["privacy.redaction_job_validation_error"]
     ] = "privacy.redaction_job_validation_error"
+
+    class ErroringObject(StripeObject):
+        id: str
+        """
+        Unique identifier for the object.
+        """
+        object_type: str
+        """
+        Erroring object type
+        """
+
     code: Literal[
         "invalid_cascading_source",
         "invalid_file_purpose",
@@ -25,9 +36,9 @@ class RedactionJobValidationError(StripeObject):
     """
     A code indicating the reason for the error.
     """
-    erroring_object: Optional[Dict[str, str]]
+    erroring_object: Optional[ErroringObject]
     """
-    If the error is related to a specific object, this field will include the object's identifier in `id` and object type in `object`.
+    If the error is related to a specific object, this field includes the object's identifier and object type.
     """
     id: str
     """
@@ -41,3 +52,4 @@ class RedactionJobValidationError(StripeObject):
     """
     String representing the object's type. Objects of the same type share the same value.
     """
+    _inner_class_types = {"erroring_object": ErroringObject}
