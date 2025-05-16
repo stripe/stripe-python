7466290f9246090a6036198a9f937b4c4f90aab7
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-05-16 13:48:56 +0000

    Update generated code for v1766

--- stripe/_quote_preview_subscription_schedule.py
+++ stripe/_quote_preview_subscription_schedule.py
@@ -466,7 +466,7 @@ class QuotePreviewSubscriptionSchedule(StripeObject):
             "always_invoice", "create_prorations", "none"
         ]
         """
-        If the subscription schedule will prorate when transitioning to this phase. Possible values are `create_prorations` and `none`.
+        When transitioning phases, controls how prorations are handled (if any). Possible values are `create_prorations`, `none`, and `always_invoice`.
         """
         start_date: int
         """
