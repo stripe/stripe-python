6c9ffe903c87fd8b33108d4097dd052106672df0
Prathmesh Ranaut <165320323+prathmesh-stripe@users.noreply.github.com>
2025-09-29 14:25:08 -0400

    Fixed InvoiceLineItem method definition (#1604)
    * Fixed InvoiceLineItem method definition
    
    * Fixed InvoiceLineItem method definition

--- tests/api_resources/test_invoice_line_item.py
+++ tests/api_resources/test_invoice_line_item.py
@@ -2,6 +2,7 @@ import stripe
 
 
 TEST_INVOICE_ID = "in_123"
+TEST_LINE_ITEM_ID = "il_123"
 
 
 class TestInvoiceLineItem(object):
@@ -9,3 +10,16 @@ class TestInvoiceLineItem(object):
         invoice = stripe.Invoice.retrieve(TEST_INVOICE_ID)
         assert isinstance(invoice.lines.data, list)
         assert isinstance(invoice.lines.data[0], stripe.InvoiceLineItem)
+
+    def test_is_modifiable(self, http_client_mock):
+        resource = stripe.InvoiceLineItem.modify(
+            TEST_INVOICE_ID, TEST_LINE_ITEM_ID, metadata={"key": "value"}
+        )
+
+        http_client_mock.assert_requested(
+            "post",
+            path="/v1/invoices/%s/lines/%s"
+            % (TEST_INVOICE_ID, TEST_LINE_ITEM_ID),
+            post_data="metadata[key]=value",
+        )
+        assert isinstance(resource, stripe.InvoiceLineItem)
