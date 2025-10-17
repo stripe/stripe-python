3462768b5e5bdbdfeea5b4b656e71e7efb2d5924
Prathmesh Ranaut <prathmesh@stripe.com>
2025-10-17 12:55:45 -0400

    Added a missing import

--- stripe/v2/core/__init__.py
+++ stripe/v2/core/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+
+from typing_extensions import TYPE_CHECKING
 from stripe.v2.core._event import (
     EventNotification as EventNotification,
     RelatedObject as RelatedObject,
