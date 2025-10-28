95303e0a154f0a813913c8670d2d833b5e869a96
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-10-28 19:49:16 +0000

    Update generated code for v2103 and

--- stripe/params/_webhook_endpoint_create_params.py
+++ stripe/params/_webhook_endpoint_create_params.py
@@ -125,7 +125,6 @@ class WebhookEndpointCreateParams(RequestOptions):
             "2025-08-27.basil",
             "2025-09-30.clover",
             "2025-10-29.clover",
-            "2025-11-17.clover",
         ]
     ]
     """
