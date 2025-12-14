767807a2aeec1a65e61169f2fe0b4bde0e6c100e
Praniket Walavalkar <praniketkw@gmail.com>
2025-12-13 21:27:26 -0800

    Suppress deprecation warnings during internal encoding to fix metadata updates

--- stripe/_encode.py
+++ stripe/_encode.py
@@ -1,6 +1,7 @@
 import calendar
 import datetime
 import time
+import warnings
 from collections import OrderedDict
 from typing import Generator, Tuple, Any
 
@@ -31,30 +32,35 @@ def _api_encode(data) -> Generator[Tuple[str, Any], None, None]:
     for key, value in data.items():
         if value is None:
             continue
-        elif (
-            hasattr(value, "get")
-            and callable(value.get)
-            and value.get("id") is not None
-        ):
-            # Check if value is a StripeObject with an 'id' without triggering deprecated stripe_id property
-            yield (key, value.get("id"))
-        elif isinstance(value, list) or isinstance(value, tuple):
-            for i, sv in enumerate(value):
-                # Always use indexed format for arrays
-                encoded_key = "%s[%d]" % (key, i)
-                if isinstance(sv, dict):
-                    subdict = _encode_nested_dict(encoded_key, sv)
-                    for k, v in _api_encode(subdict):
-                        yield (k, v)
-                else:
-                    yield (encoded_key, sv)
-        elif isinstance(value, dict):
-            subdict = _encode_nested_dict(key, value)
-            for subkey, subvalue in _api_encode(subdict):
-                yield (subkey, subvalue)
-        elif isinstance(value, datetime.datetime):
-            yield (key, _encode_datetime(value))
-        elif isinstance(value, bool):
-            yield (key, str(value).lower())
         else:
-            yield (key, value)
+            # Check if value has stripe_id attribute, suppressing deprecation warnings
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", DeprecationWarning)
+                has_stripe_id = hasattr(value, "stripe_id")
+
+            if has_stripe_id:
+                with warnings.catch_warnings():
+                    warnings.simplefilter("ignore", DeprecationWarning)
+                    yield (key, value.stripe_id)
+                continue
+
+            if isinstance(value, list) or isinstance(value, tuple):
+                for i, sv in enumerate(value):
+                    # Always use indexed format for arrays
+                    encoded_key = "%s[%d]" % (key, i)
+                    if isinstance(sv, dict):
+                        subdict = _encode_nested_dict(encoded_key, sv)
+                        for k, v in _api_encode(subdict):
+                            yield (k, v)
+                    else:
+                        yield (encoded_key, sv)
+            elif isinstance(value, dict):
+                subdict = _encode_nested_dict(key, value)
+                for subkey, subvalue in _api_encode(subdict):
+                    yield (subkey, subvalue)
+            elif isinstance(value, datetime.datetime):
+                yield (key, _encode_datetime(value))
+            elif isinstance(value, bool):
+                yield (key, str(value).lower())
+            else:
+                yield (key, value)
